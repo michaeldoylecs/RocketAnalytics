d21cd1be3168d2c48e500a35afb0ae7f4bde800d
Michael Doyle <michaeldoylecs@gmail.com>
2018-02-04 19:45:27 -0500

    ADD File and function documentation

--- RocketAnalyticsLib/include/properties/ByteValue.hpp
+++ RocketAnalyticsLib/include/properties/ByteValue.hpp
@@ -1,25 +1,55 @@
-// Author: Michael Doyle
-// Date: 8/12/17
-// ByteValue.hpp
+/******************************************************************************
+*	Author:	Michael Doyle
+*	Date:	8/12/17
+*	File:	ByteValue.hpp
+*	Description:
+*		ByteValue represents the 'ByteValue' property type found in Rocket
+*	League replay files. The name does not have any connection to it's size or
+*	inner workings.
+******************************************************************************/
 
 #ifndef BYTE_VALUE_H
 #define BYTE_VALUE_H
 
 #include <string>
 
+using std::string;
+
 namespace ReplayParser {
 
 	class ByteValue {
 		public:
+			/*
+			 *	Default constructor.
+			 */
 			ByteValue();
+
+			/*
+			 *	Constructs a copy of a given ByteValue.
+			 */
 			ByteValue(const ByteValue &byte);
-			ByteValue(std::string identifier, std::string value);
-			std::string get_identifer();
-			std::string get_value();
+
+			/*
+			 *	Constructs a ByteValue given its identifier and value.
+			 */
+			ByteValue(string identifier, string value);
+
+			/*
+			 *	Returns the ByteValue's identifer.
+			 */
+			string get_identifer();
+
+			/*
+			 *	Returns the ByteValue's value.
+			 */
+			string get_value();
 
 		private:
-			std::string byte_identifier;
-			std::string byte_value;
+			/*
+			 *	ByteValue data members.
+			 */
+			string byte_identifier;
+			string byte_value;
 	};
 	
 }
