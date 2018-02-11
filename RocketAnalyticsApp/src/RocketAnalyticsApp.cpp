0aabe5e4a2eb8d3d7c041155810c9875fb461303
Michael Doyle <mcdoyle7227@gmail.com>
2018-02-11 00:10:49 -0500

    Add formatting

--- RocketAnalyticsApp/src/RocketAnalyticsApp.cpp
+++ RocketAnalyticsApp/src/RocketAnalyticsApp.cpp
@@ -18,9 +18,10 @@ void test_binary_reader(const string file_path) {
 		BinaryReader b_reader(file_path);
 		cout << "Headersize: " << b_reader.read_aligned_uint32() << endl;
 		cout << "CRC: " << b_reader.read_aligned_uint32() << endl;
-		cout << "Version: " << b_reader.read_aligned_uint32() << "."
-			<< b_reader.read_aligned_uint32() << endl;
-		cout << "ReplayIdentifier: " << b_reader.read_length_prefixed_string() << endl;
+		cout << "Version: " << b_reader.read_aligned_uint32() << "." <<
+			b_reader.read_aligned_uint32() << endl;
+		cout << "ReplayIdentifier: " <<
+			b_reader.read_length_prefixed_string() << endl;
 		cout << "---END TESTING BinaryReader---" << "\n" << endl;
 	}
 	catch (const std::runtime_error &e) {
@@ -34,16 +35,21 @@ void test_replay(const string file_path) {
 		cout << "Filepath: " << file_path << endl;
 		ReplayFile replay = ReplayFile(file_path);
 
-		cout << "Header size: " << replay.get_header().get_header_size() << endl;
+		cout << "Header size: " <<
+			replay.get_header().get_header_size() << endl;
 		cout << "CRC1: " << replay.get_header().get_crc1() << endl;
 		cout << "Version: " << replay.get_header().get_version() << endl;
-		cout << "Version major: " << replay.get_header().get_version_major() << endl;
-		cout << "Version minor: " << replay.get_header().get_version_minor() << endl;
-		cout << "Replay Identifier: " << replay.get_header().get_replay_identifier() << endl;
+		cout << "Version major: " <<
+			replay.get_header().get_version_major() << endl;
+		cout << "Version minor: " <<
+			replay.get_header().get_version_minor() << endl;
+		cout << "Replay Identifier: " <<
+			replay.get_header().get_replay_identifier() << endl;
 
 		cout << "Properties:" << endl;
 		for (int i = 0; i < replay.get_header().get_properties().size(); i++) {
-			cout << " > " << replay.get_header().get_properties().at(i).to_string() << endl;
+			cout << " > " <<
+				replay.get_header().get_properties().at(i).to_string() << endl;
 		}
 
 		cout << "Body Size: " << replay.get_header().get_body_size() << endl;
@@ -63,7 +69,8 @@ void test_replay(const string file_path) {
 
 int main() {
 	try {
-		const string file_path = "../Testing/0A797CAB49E97F824000D9BB757BF7F9.replay";
+		const string file_path =
+			"../Testing/0A797CAB49E97F824000D9BB757BF7F9.replay";
 		test_binary_reader(file_path);
 		test_replay(file_path);
 	}
