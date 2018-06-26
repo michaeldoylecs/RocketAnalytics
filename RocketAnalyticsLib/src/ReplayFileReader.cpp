6036fae0709e9236580c5e22134c61c792a56281
Michael Doyle <mcdoyle7227@gmail.com>
2018-06-26 18:07:06 -0400

    Change error reporting from std::cout to std::cerr

--- RocketAnalyticsLib/src/ReplayFileReader.cpp
+++ RocketAnalyticsLib/src/ReplayFileReader.cpp
@@ -13,7 +13,7 @@ namespace ReplayParser {
     this->replay_file_stream.open(filepath, std::ios::binary);
     this->replay_file_path = filepath;
     if (this->replay_file_stream.is_open()) {
-      std::cout << "Failed to open file stream for '" << replay_file_path << "'";
+      std::cerr << "Failed to open file stream for '" << replay_file_path << "'";
     }
   }
 
@@ -30,7 +30,7 @@ namespace ReplayParser {
       "ReplayFileReader::read_int8 requires int8_t to be 1 byte");
     std::int8_t raw_int = 0;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_int;
     }
     replay_file_stream.read(reinterpret_cast<char *>(&raw_int), sizeof(raw_int)); // NOLINT
@@ -44,7 +44,7 @@ namespace ReplayParser {
       "ReplayFileReader::read_int32 requires int32_t to be 4 bytes");
     std::int32_t raw_int = 0;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_int;
     }
     replay_file_stream.read(reinterpret_cast<char *>(&raw_int), sizeof(raw_int)); // NOLINT
@@ -58,7 +58,7 @@ namespace ReplayParser {
       "ReplayFileReader::read_int64 requires int64_t to be 8 bytes");
     std::int64_t raw_int = 0;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_int;
     }
     replay_file_stream.read(reinterpret_cast<char *>(&raw_int), sizeof(raw_int)); // NOLINT
@@ -72,7 +72,7 @@ namespace ReplayParser {
       "ReplayFileReader::read_float requires float to be 4 bytes");
     float raw_float = 0.0;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_float;
     }
     replay_file_stream.read(reinterpret_cast<char *>(&raw_float), sizeof(raw_float)); // NOLINT
@@ -86,7 +86,7 @@ namespace ReplayParser {
       "ReplayFileReader::read_bool requires bool to be 1 byte");
     bool raw_bool = false;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_bool;
     }
     replay_file_stream.read(reinterpret_cast<char *>(&raw_bool), sizeof(raw_bool)); // NOLINT
@@ -96,7 +96,7 @@ namespace ReplayParser {
   std::string ReplayFileReader::read_string(int length) {
     std::string raw_string;
     if (!replay_file_stream) {
-      std::cout << "File stream not open for '" << replay_file_path << "'";
+      std::cerr << "File stream not open for '" << replay_file_path << "'";
       return raw_string;
     }
     raw_string.resize(length);
